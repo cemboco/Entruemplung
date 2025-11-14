ff1409cac95eebb24d5f4577cf129bb63c935411
Cem04 <45462437+cemboco@users.noreply.github.com>
2025-11-14 19:18:50 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
-import About from './components/About';
 import Process from './components/Process';
 import Pricing from './components/Pricing';
 import Coverage from './components/Coverage';
@@ -16,7 +15,6 @@ function App() {
       <Header />
       <Hero />
       <Services />
-      <About />
       <Process />
       <Pricing />
       <Coverage />
