f0fe9d5670f9ab481449ff2048a8750a28d3596f
Cem04 <45462437+cemboco@users.noreply.github.com>
2025-11-15 19:34:43 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import FAQ from './components/FAQ';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import CallToActionPopup from './components/CallToActionPopup';
+import FeedbackTab from './components/FeedbackTab';
 
 function App() {
   useEffect(() => {
@@ -30,6 +31,7 @@ function App() {
       <Contact />
       <Footer />
       <CallToActionPopup />
+      <FeedbackTab />
     </div>
   );
 }
