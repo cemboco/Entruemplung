457b8d5204a6ed1aee9cd6ec7ca4c251b3d357c9
Cem04 <45462437+cemboco@users.noreply.github.com>
2026-01-25 15:36:42 +0100

    Remove url parameter from render function

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,7 +2,7 @@ import { renderToString } from 'react-dom/server';
 import { MemoryRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 
-export function render(url: string) {
+export function render() {
   const html = renderToString(
     <MemoryRouter initialEntries={['/']}>
       <AppRoutes />
