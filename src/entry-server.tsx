56018be7550d07051468c4f2202ab7c5386a5c59
Cem04 <45462437+cemboco@users.noreply.github.com>
2026-01-25 16:27:24 +0100

    Change render function parameter type to string

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,7 +2,7 @@ import { renderToString } from 'react-dom/server';
 import { MemoryRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 
-export function render(url = '/') {
+export function render(url: string) {
   const html = renderToString(
     <MemoryRouter initialEntries={[url]}>
       <AppRoutes />
