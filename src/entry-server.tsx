002e4d3cd54c761f4eed2266a3517231abded837
Cem04 <45462437+cemboco@users.noreply.github.com>
2026-01-25 16:33:11 +0100

    Refactor render function in entry-server.tsx

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,8 +1,1 @@
-export function render() {
-  const html = renderToString(
-    <MemoryRouter initialEntries={['/']}>
-      <AppRoutes />
-    </MemoryRouter>
-  );
-  return html;
-}
+import { renderToString } from 'react-dom/server'; import { MemoryRouter } from 'react-router-dom'; import AppRoutes from './AppRoutes'; export function render() { const html = renderToString( <MemoryRouter initialEntries={['/']}> <AppRoutes /> </MemoryRouter> ); return html; }
